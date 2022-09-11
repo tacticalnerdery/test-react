2696870075176beaaa77cb1966ffe8d4d9cf3ee8
Ray Terrill <rayjohnterrill@gmail.com>
2022-09-11 11:07:43 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import { React , useState } from 'react';
 import './App.css';
-import Toast from './Toast';
+import Refresh from './Refresh.js';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
+
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://cra.link/PWA
+  //serviceWorkerRegistration.register();
+  serviceWorkerRegistration.register({
+    onUpdate: registration => {
+      console.log("update!")
+      setShowAlert(true);
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload 4.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container fluid>
+      <div className="border d-flex align-items-center justify-content-center vh-100">
+      <Card style={{ width: '18rem' }}>
+        <Card.Header>
+          TEST
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </div>
+    </Container>
+    <Refresh showAlert={showAlert} />
+    </>
   );
 }
 
